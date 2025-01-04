aa1b06e1fff6c2fd9b45f537a02048ad988590d7
zhouchengping <80532161+zhouchengping@users.noreply.github.com>
2025-01-04 14:43:46 +0800

    Add files via upload

---
+++ SelfData/test.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+from network import ShuffleNetV1  # 需要你自己提供ShuffleNetV1的模型定义文件
+
+model_path = 'models/checkpoint-005000.pth.tar'  # 替换为训练后的 .pth.tar 文件路径
+checkpoint = torch.load(model_path)
+print(checkpoint['state_dict'].keys())  # 打印出模型中包含的所有键
+# 打印文件内容，查看其键（key）
+print(11)
+print(checkpoint.keys())
+print(checkpoint['state_dict'].keys())
+print('/n')
+print(checkpoint['state_dict']['module.first_conv.1.weight'])
